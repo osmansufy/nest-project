289d24fb87b564cdef02f6c756e21b0fb0fd41ea
osmansufy <osmansufy20@gmail.com>
2023-01-28 23:42:12 +0600

    Start:project

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller()
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('signup')
+  signUp() {
+    return this.authService.signUp();
+  }
+  @Post('signin')
+  signIn() {}
+}
